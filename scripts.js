91d00075e4e7cf6b5f6d7879dc6185c08502cc4a
matt2112 <matt_lewis@live.co.uk>
2016-07-06 22:33:44 +0100

    improve layout, rearrange html and js code

--- scripts.js
+++ scripts.js
@@ -1,5 +1,14 @@
+"use strict";
+
+/**
+ * Global variables.
+ **/
 var player = "O";
 var computer = "X";
+var move = 0;
+var winner = false;
+var choose = false;
+var inProgress = false;
 var grid = {
     r1c1: null,
     r1c2: null,
@@ -10,49 +19,123 @@ var grid = {
     r3c1: null,
     r3c2: null,
     r3c3: null
-}
+};
 var keys = Object.keys(grid);
-move = 0;
 
-$("#chooseO").on("click", function() {
-    player = "O";
-    computer = "X";
-})
+/**
+ * Awaits user click to begin game.
+ **/
+$("#start").on("click", function () {
+    if (!inProgress) {
+        $("#instructions").html("Choose X or O (X begins).");
+        choose = true;
+    }
+});
 
-$("#chooseX").on("click", function() {
-    player = "X";
-    computer = "O";
-})
-
-$("td").on("click", function() {
-    var cell = $(this)[0].id;
-    if (grid[cell] === null) {
-        $(this).text(player);
-        grid[cell] = player;
-        if (move < 4) {
-            $computerMove();   
+/** 
+ * Allows user to select shape once at beginning of game.
+ **/
+$(".choice").on("click", function () {
+    if (choose) {
+        player = $(this).text();
+        computer = (player === "O") ? "X" : "O";
+        choose = false;
+        inProgress = true;
+        if (player === "O") {
+            computerMove();
         }
     }
-})
+});
 
-$("#clear").on("click", function() {
+/**
+ * Draws symbol on board.
+ **/
+$("td").on("click", function () {
+    if (inProgress) {
+        var cell = $(this)[0].id;
+        if ((grid[cell] === null) && (!winner)) {
+            $(this).text(player);
+            grid[cell] = player;
+            calculateWin();
+            if ((move < 4) && (!winner)) {
+                computerMove();
+                calculateWin();
+            }
+        }
+    }
+
+});
+
+/**
+ * Clears board and resets variables.
+ **/
+$("#reset").on("click", function () {
     $("td").text(" ");
     for (var i = 0; i < keys.length; i++) {
         grid[keys[i]] = null;
     }
     move = 0;
-})
+    winner = false;
+});
 
-$computerMove = function() {
-    console.log("start");
+// computer moves randomly
+var computerMove = function () {
     var cell = keys[Math.floor(Math.random() * keys.length)];
-    console.log(cell);
     if (grid[cell] !== null) {
-        console.log("oops");
-        $computerMove();
+        computerMove();
     } else {
         grid[cell] = computer;
         $("#" + cell).text(computer);
         move += 1;
     }
-};
+}
+
+// looks for a winning board and responds accordingly
+var calculateWin = function () {
+    // check first row
+    if ((grid["r1c1"] !== null) && (grid["r1c1"] === grid["r1c2"]) && (grid["r1c1"] === grid["r1c3"])) {
+        winResponse();
+    }
+
+    //check second row
+    if ((grid["r2c1"] !== null) && (grid["r2c1"] === grid["r2c2"]) && (grid["r2c1"] === grid["r2c3"])) {
+        winResponse();
+    }
+
+    //check third row
+    if ((grid["r3c1"] !== null) && (grid["r3c1"] === grid["r3c2"]) && (grid["r3c1"] === grid["r3c3"])) {
+        winResponse();
+    }
+
+    // check first column
+    if ((grid["r1c1"] !== null) && (grid["r1c1"] === grid["r2c1"]) && (grid["r1c1"] === grid["r3c1"])) {
+        winResponse();
+    }
+
+    // check second column
+    if ((grid["r1c2"] !== null) && (grid["r1c2"] === grid["r2c2"]) && (grid["r1c2"] === grid["r3c2"])) {
+        winResponse();
+    }
+
+    // check third column
+    if ((grid["r1c3"] !== null) && (grid["r1c3"] === grid["r2c3"]) && (grid["r1c3"] === grid["r3c3"])) {
+        winResponse();
+    }
+
+    // check first diagonal
+    if ((grid["r1c1"] !== null) && (grid["r1c1"] === grid["r2c2"]) && (grid["r1c1"] === grid["r3c3"])) {
+        winResponse();
+    }
+
+    // check second diagonal
+    if ((grid["r1c3"] !== null) && (grid["r1c3"] === grid["r2c2"]) && (grid["r1c3"] === grid["r3c1"])) {
+        winResponse();
+    }
+}
+
+// responds to winning board
+var winResponse = function () {
+    alert("winner!");
+    winner = true;
+    inProgress = false;
+}
